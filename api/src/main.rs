bbcb68f31d6bf31bf38957e4997f7051a2acc91f
U-DESKTOP-7L1HLDO\Machadza <krzysztof.baraszek@wp.pl>
2025-01-26 22:24:23 +0100

    hongkong

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,10 @@
 use actix_cors::Cors;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use futures_util::{Stream, StreamExt, TryStreamExt};
+use futures_util::{lock::Mutex, task, Stream, StreamExt, TryStreamExt};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
+use tokio::time;
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 const API_KEY: &str = "pg1XbIBTiRC5WZaWrDuMQzSbphnchVOM";
 
@@ -30,6 +32,26 @@ struct TickerEntry {
     pub close: f32,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct PrevResult{
+    pub T: String,
+    pub o: f32,
+    pub c: f32
+}
+
+impl From <PrevResult> for TickerEntry {
+    fn from(value: PrevResult) -> Self {
+        TickerEntry{symbol: value.T, open: value.o, close: value.c}
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PrevResponse{
+    pub results: Vec<PrevResult>
+}
+
+
 #[get("/")]
 async fn hello() -> impl Responder {
     let url = format!(
@@ -43,62 +65,55 @@ async fn hello() -> impl Responder {
 }
 
 #[get("/tabledata")]
-async fn tabledata() -> impl Responder {
-    let yesterday = chrono::Local::now().checked_sub_days(chrono::Days::new(1)).unwrap();
-    let data: Vec<_> = futures_util::stream::iter(TICKERS.iter().take(1))
-        .then(|symbol| async move {
-            let url = format!(
-                "https://api.polygon.io/v1/open-close/{}/{}?apiKey={}",
-                symbol,
-                yesterday.format("%Y-%m-%d"),
-                API_KEY
-            );
-            let res = reqwest::get(url).await.unwrap();
-            if res.status() == StatusCode::OK {
-                let entry: TickerEntry = res.json().await.unwrap();
-                Ok(entry)
-            } else {
-                Err(res.text().await.unwrap())
-            }
+async fn tabledata(data: web::Data<Mutex<HashMap<String, TickerEntry>>>) -> impl Responder {
+    let lock = data.lock().await;
+    let values = lock.iter().map(|(k, v)| v.to_owned()).collect::<Vec<_>>();
+    HttpResponse::Ok().json(values)
+}
 
-            //todo
-            // funkcja w tokio co robi co pewien czas (24h) Interval spawni taski do pobieranie tych jebanych danych do tabelki
-            
-            }).filter(|res| {let ret = res.is_ok(); async move {ret}})
-        .try_collect()
-        .await.unwrap();
-    // let file = File::open("DATA.json").expect("msg");
-    // let data: serde_json::Value = serde_json::from_reader(file).expect("cos");
-    println!("{:#?}", &data);
-    HttpResponse::Ok().json(data)
+async fn update(data: &mut Arc<Mutex<HashMap<String, TickerEntry>>>) {  
+    for x in TICKERS.into_iter() {
+        let mut lock = data.lock().await;  
+
+        let url = format!(
+            "https://api.polygon.io/v2/aggs/ticker/{}/prev?apiKey={}",
+            x,
+            API_KEY
+        );
+
+        let res: reqwest::Response = reqwest::get(url).await.unwrap();
+        let mut entry: PrevResponse = res.json().await.unwrap();
+        let ticker_entry: TickerEntry = entry.results.pop().unwrap().into();
+        lock.insert(x.to_owned(), ticker_entry);
+        drop(lock);
+        tokio::time::sleep(Duration::from_secs(13)).await;
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let url = format!(
-    //     "{}{}",
-    //     "https://api.polygon.io/v3/reference/tickers?&active=true&limit=1000&apiKey=", API_KEY
-    // );
-    // let res = reqwest::get(url).await.unwrap();
-    // let tickers = res
-    //     .json::<TickersResponse>()
-    //     .await
-    //     .map(|result| {
-    //         result
-    //             .results
-    //             .into_iter()
-    //             .map(|ticker| ticker.ticker)
-    //             .collect::<Vec<_>>()
-    //     })
-    //     .unwrap();
+    let data: Arc<Mutex<HashMap<String, TickerEntry>>> = Arc::new(Mutex::new(HashMap::new()));
 
-    // println!("{:?}",tickers);
+    let data_clone = data.clone();
 
-    HttpServer::new(|| {
+    let update_tabledata = tokio::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(70));
+        let mut data = data_clone;
+
+        loop {
+            interval.tick().await;
+            update(&mut data).await;
+        }
+    });
+
+    let data = web::Data::from(data);
+
+    HttpServer::new(move || {
         let cors = Cors::permissive();
-        App::new().wrap(cors).service(hello).service(tabledata)
+        App::new().app_data(data.clone()).wrap(cors).service(hello).service(tabledata)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { DataTable} from './assets/Stocks/DataTable'
 
+function plus(a) {
+  if(a > 0)
+    return '+' 
+}
+
 function App() {
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState([])
@@ -15,12 +20,13 @@ function App() {
         .then(json => setData(json))
         .finally(()=> {
           setLoading(false)
-        })}, 12000)
+        })}, 1000)
       return () => clearInterval(interval)
   }, [])
 
   return (
     <div className="App">
+
       {loading ? (
         <div>Loading...</div>
       ) : (
@@ -36,9 +42,10 @@ function App() {
             {data.map(data => (
               <tr key={data.symbol}>
                 <td>{data.symbol}</td>
+                <td>{plus(data.close - data.open)}{(Math.round(data.close - data.open)/data.open * 100).toFixed(2)}%</td>
                 <td>{data.open}</td>
-                <td>{data.open}</td>
-                <td>{data.close}</td>               
+                <td>{data.close}</td>    
+                <td><button onClick={() => {console.log(data.symbol)}}>wykres</button></td>        
               </tr>
             ))}
           </table>
