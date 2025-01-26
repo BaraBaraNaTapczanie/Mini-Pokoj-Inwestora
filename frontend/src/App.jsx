c46e88adaa7ab294099d8f60f566d9bab643db29
U-DESKTOP-7L1HLDO\Machadza <krzysztof.baraszek@wp.pl>
2025-01-26 22:28:04 +0100

    ale kongo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './assets/table.css'
 import './App.css'
-import { DataTable} from './assets/Stocks/DataTable'
 
 function plus(a) {
   if(a > 0)
